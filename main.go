67caab97a78cf649b4fc2be150e7331bbf7c88d3
Charles Pigott <charlespigott@googlemail.com>
2016-01-02 10:55:58 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+
+	"github.com/docopt/docopt-go"
+	"gopkg.in/yaml.v2"
+)
+
+//  pisg [-ch channel] [-l logfile] [-o outputfile] [-ma maintainer]
+//       [-f format] [-ne network] [-d logdir] [-mo moduledir] [-s] [-v] [-h]
+
+func ParseArgs() map[string]interface{} {
+	usage := `Go IRC Statistics Generator.
+
+Usage:
+  gisg -c CFG [-s]
+
+Options:
+  -c --config-file=CFG  Configuration file [default: gisg.yaml].
+  -s  --silent          Suppress output (except error messages).
+  -v  --version         Show version.
+  -h  --help            Output this message and exit.`
+
+	arguments, _ := docopt.Parse(usage, nil, true, "GISG 0.1", false)
+	return arguments
+}
+
+type logConfig struct {
+	Network    string
+	Format     string
+	InputFile  string
+	OutputFile string
+}
+
+type yamlConfig struct {
+	Maintainer string
+	Channels   map[string]logConfig
+}
+
+func main() {
+	args := ParseArgs()
+
+	cfgData, err := ioutil.ReadFile(args["--config-file"].(string))
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	var cfg yamlConfig
+	if err = yaml.Unmarshal(cfgData, &cfg); err != nil {
+		log.Fatalln(err.Error())
+	}
+	log.Println(cfg)
+}
